fd8a7dd57a3168a05071c8e84c0a7a1c9cb7024b
w-mj <w.mingjian@yahoo.com>
2019-01-19 20:22:42 +0800

    static

--- website/settings.py
+++ website/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, "static").replace('\\', '/')
+
 
 
 MEDIA_URL = '/media/'
