415ccac18a5f87b936b8aa1629d671efca33943c
w-mj <w.mingjian@yahoo.com>
2019-01-19 20:28:04 +0800

    static url

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git checkout -- .
 git pull origin master
+python3 manage.py collectstatic
 uwsgi --reload uwsgi.pid
--- website/urls.py
+++ website/urls.py
@@ -36,4 +36,6 @@ urlpatterns = [
     path('chat', chatbot.func.chat),
     path('separation', chatbot.func.separation),
     path('deploy', deploy)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
