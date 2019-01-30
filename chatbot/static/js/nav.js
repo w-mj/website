451317f74128fad0f392b776261484ca039f8872
w-mj <w.mingjian@yahoo.com>
2019-01-30 13:58:26 +0800

    optimize speed

--- chatbot/static/js/nav.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("#user-btn-group").hover(function () {
-        $("#user-btn-group").prepend("<button type=\"button\" class=\"btn btn-outline-danger\" style=\"display: none;\">退出登陆</button>\n");
-    }, function () {
-        $("#user-btn-group").html("<button type=\"button\" class=\"btn btn-outline-success\" id=\"user-btn\">Middle</button>\n")
-    })
-});
