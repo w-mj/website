ec2a3788b41da154d6686a284dc4baa5be51578f
w-mj <w.mingjian@yahoo.com>
2019-03-23 10:45:30 +0800

    change database to mysql

--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import socket
+import pymysql
+pymysql.install_as_MySQLdb()
 from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -77,9 +79,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': './website/my.cnf',
+        },
     }
 }
 
