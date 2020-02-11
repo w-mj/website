9a9cf222b7764f5859c017232338439498258278
Mingjian W <w.mingjian@yahoo.com>
2020-02-11 12:14:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 git checkout -- .
 git pull origin master
 python3 manage.py collectstatic --noinput
-ssh -i ~/Login_tencent root@127.0.0.1 "supervisorctl restart website"
+ssh root@127.0.0.1 "supervisorctl restart chatbot"
